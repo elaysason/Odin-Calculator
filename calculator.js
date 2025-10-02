1792d6fe3db14eaa3b105444d2800a3f1dc191c5
Elay Sason <elaysason@gmail.com>
2025-10-02 16:28:53 +0300

    finished calculator - added css and finished basic logic

--- calculator.js
+++ calculator.js
@@ -15,20 +15,107 @@ function multiply(number1, number2) {
 }
 
 function divide(number1, number2) {
+  if (number2 === 0) {
+    alert("Can't divide by zero :(");
+    return 0;
+  }
+
   return number1 / number2;
 }
 
 function operate(opreator, number1, number2) {
+  console.log(opreator, number1, number2);
   switch (opreator) {
-    case "+":
-      return add(number1, number2);
-    case "-":
-      return subtract(number1, number2);
-    case "*":
-      return multiply(number1, number2);
-    case "/":
-      return divide(number1, number2);
+    case "add":
+      return Math.round(add(+number1, +number2) * 100000) / 100000;
+    case "minus":
+      return Math.round(subtract(+number1, +number2) * 100000) / 100000;
+    case "multi":
+      return Math.round(multiply(+number1, +number2) * 100000) / 100000;
+
+    case "divide":
+      return Math.round(divide(+number1, +number2) * 100000) / 100000;
+    case "equal":
+      return number1;
     default:
-      return -1;
+      console.error("Unrequnised opreation");
   }
 }
+const all_buttons = Array.from(document.querySelectorAll("button"));
+const numberButtons = all_buttons.filter(
+  (button) => !isNaN(button.textContent)
+);
+const display_input = document.querySelector("input");
+let number1 = "";
+let number2 = "";
+let cur_opreation = "";
+let cur_number = "";
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (cur_number === "") {
+      display_input.value = "";
+    }
+    display_input.value += button.textContent;
+    cur_number += button.textContent;
+  });
+});
+let opreations = ["multi", "minus", "add", "divide"];
+opreations.forEach((opreation) => {
+  let button = document.querySelector("#" + opreation);
+
+  button.addEventListener("click", () => {
+    if (cur_opreation === opreation) return;
+    opreations.forEach((resetOp) => {
+      const btn = document.querySelector("#" + resetOp);
+      btn.style.backgroundColor = ""; // or default color
+    });
+
+    button.style.backgroundColor = "lightblue";
+    if (number1 === "") {
+      number1 = display_input.value;
+      cur_opreation = opreation;
+      console.log(opreation);
+      cur_number = "";
+    } else {
+      if (number2 === "") {
+        number2 = display_input.value;
+        number1 = operate(cur_opreation, number1, number2);
+        display_input.value = number1;
+        cur_opreation = opreation;
+        cur_number = "";
+      } else {
+        number2 = cur_number;
+        number1 = operate(cur_opreation, number1, number2);
+        display_input.value = number1;
+        cur_opreation = opreation;
+        cur_number = "";
+      }
+    }
+  });
+});
+clear_button = document.querySelector("#clear");
+clear_button.addEventListener("click", () => {
+  number1 = "";
+  number2 = "";
+  cur_number = "";
+  cur_opreation = "";
+  display_input.value = "";
+  opreations.forEach((resetOp) => {
+    const btn = document.querySelector("#" + resetOp);
+    btn.style.backgroundColor = ""; // or default color
+  });
+});
+equal_button = document.querySelector("#equal");
+equal_button.addEventListener("click", () => {
+  opreations.forEach((resetOp) => {
+    const btn = document.querySelector("#" + resetOp);
+    btn.style.backgroundColor = ""; // or default color
+  });
+  if (number1 != "" && cur_opreation != "") {
+    number2 = display_input.value;
+    number1 = operate(cur_opreation, number1, number2);
+    display_input.value = number1;
+    cur_opreation = "equal";
+    cur_number = "";
+  }
+});
